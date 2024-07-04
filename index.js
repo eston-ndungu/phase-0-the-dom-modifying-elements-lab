dd9064eb0642596c95d7f021eeab3802313a536a
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-04 19:47:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+
+// Selects the first element that matches a CSS selector
+document.querySelector('#main').remove()
+
+// Creates a new HTML element
+const newHeader = document.createElement('h1')
+
+// Set an attribute of id to the newHeader and assign it a value 
+newHeader.setAttribute('id', 'victory');
+
+
+newHeader.textContent = 'ESTON is the champion'
+
+//Adds the above Element to the DOM
+document.head.append(newHeader);
+
+
+
+
+
+
